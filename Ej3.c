35bafb11f3c56daf87dbd67ee87e3348ab770065
vicdeaj <al404303@uji.es>
2021-11-29 13:58:32 +0100

    unfinished

--- Ej3.c
+++ Ej3.c
@@ -1,4 +1,19 @@
-//
-// Created by usuario on 29/11/21.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
+int main(int argc, char* args[]) {
+    if (argc != 2){
+        printf("Incorrect number of arguments\n");
+        exit(-1);
+    }
+    mkdir(args[1],0775);
+
+
+    struct dirent *d = readdir(opendir(args[1]));
+
+
+
+    exit(0);
+}
